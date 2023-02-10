1b0cc160133cd6bf1c4c34d27c547a603e427289
Enzzo Cansani <82479292+cansani@users.noreply.github.com>
2023-02-10 14:50:59 -0300

    updt script.js

--- script.js
+++ script.js
@@ -7,37 +7,76 @@ const fillForm = (data) => {
   document.getElementById('estado').value = data.uf
 }
 
-const clearForm = (data) => {
+const onlyNumbers = (e) => {
+  const keyCode = (e.keyCode ? e.keyCode : e.wich)
+
+  if(keyCode > 47 && keyCode < 58) {
+    return keyCode
+  } else {
+    e.preventDefault()
+  }
+}
+
+const clearForm = () => {
   document.getElementById('endereco').value = ''
   document.getElementById('bairro').value = ''
   document.getElementById('cidade').value = ''
   document.getElementById('estado').value = ''
 }
 
-const isNum = (n) => /^[0-9]+$/.test(n)
-
-const isValid = (cep) => cep.length == 8 && isNum(cep)
-
-const searchCEP = async() => {
-  const cep = document.getElementById('cep').value
+const searchCEP = async () => {
+  const cep = inputCEP.value
   const url = `https://viacep.com.br/ws/${cep}/json/`
   
-  if (isValid(cep)){
-    const res = await fetch(url)
+  if (document.getElementById('cep').value.length < 8) {
+    alert('CEP Invalido!')
+  } else {
+    const res =  await fetch(url)
     const data = await res.json()
-  
-  
-    if (data.hasOwnProperty('erro')) {
+
+    if (data.erro === true) {
       alert('CEP Invalido!')
       clearForm()
     } else {
       fillForm(data)
     }
-  } else {
-    alert('CEP Invalido!')
   }
-
 }
 
 
 inputCEP.addEventListener('focusout', searchCEP)
+inputCEP.addEventListener('keypress', onlyNumbers)
+
+
+const btn = document.getElementById('btn')
+const getLocalStorage = () => JSON.parse(localStorage.getItem('database')) ?? []
+const setLocalStorage = (database) => localStorage.setItem('database', JSON.stringify(database)) 
+
+const createPerson = (person) => {
+  const database = getLocalStorage()
+  database.push(person)
+  setLocalStorage(database)
+}
+
+const savePerson = () => {
+  const examplePerson = {
+    name: document.getElementById('nome').value,
+    email: document.getElementById('email').value,
+    cep: document.getElementById('cep').value,
+    adress: document.getElementById('endereco').value,
+    adress_number: document.getElementById('numero').value,
+    bairro: document.getElementById('bairro').value,
+    cidade: document.getElementById('cidade').value,
+    estado: document.getElementById('estado').value
+  }
+
+  if (examplePerson.name === '' || examplePerson.adress_number === '') {
+    alert('Preencha todos os campos!')
+  } else {
+    createPerson(examplePerson)
+    alert(`${examplePerson.name} foi adicionado(a) no banco de dados.`)
+  }
+}
+
+btn.addEventListener('click', savePerson)
+
